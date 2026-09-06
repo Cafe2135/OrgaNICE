8afe67b5015b65498c0e52e50193d6034f8ee97f
estavio2135 <estavio2135@gmail.com>
2026-09-06 13:35:47 +0800

    note for rammus

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,5 @@
+/* Ola rammus Erm im just doing the build bit by bit every day very busy with part time job
+so like just type here what else we need if you find things that need tweaking or stuff
+
+*/
+
